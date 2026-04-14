b7bcea8b07a5ce5d66abba3b5ef5cfc80a3dbad5
Elior <113261013@nccu.edu.tw>
2026-04-14 13:28:17 +0800

    ?

--- utils/line_bot.py
+++ utils/line_bot.py
@@ -1,16 +1,17 @@
+import os
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
-# 填入你剛剛拿到的 Access Token
-LINE_CHANNEL_ACCESS_TOKEN = "DXSy5nvM7UX/MS4JecDnho/4Xr2DoiCiTOBKAg0adCcdEyY1m+V68E/Vz1tmc+rRZKbCyziE26I+UXLmWB2F78suyplqqM95xOpfuXEpkp0DWhlb3JcWec6Pg4D4lZNfdjBsYHvxurc8LVpHpGf0VAdB04t89/1O/w1cDnyilFU="
+# 改用 os.getenv 從環境變數讀取，不再把密碼寫死在這裡！
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 def send_admin_notification(message_text):
     """
     發送通知給系統管理員 (也就是你)
     """
-    # 這裡要填入你自己的 LINE User ID (稍後會教你去哪裡拿)
+    # 你的 User ID 可以留在這裡，因為這不算機密，這只是你的「收件地址」
     admin_user_id = "Ua153013c2048f6bfb12937f93a672709"
     
     try:
